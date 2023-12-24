a1f8626f7dd7b5669abfd828106efe24ab7a3c72
wenhuizhang <wenhuzhang1@gmail.com>
2023-12-24 12:14:20 +0800

    print baseURL

--- web/src/api/init.js
+++ web/src/api/init.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { rememberToken, getValidToken } from "./token";
 
 const baseURL = process.env.REACT_APP_API_URL || "http://localhost:7000";
-
+console.log('baseURL:',baseURL,'env',process.env)
 // Create an axios instance
 const api = axios.create({
   baseURL,
