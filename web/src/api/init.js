b94e2d981f69446b7bdb92059130500d9775e6f0
wenhuzhang <wenhuzhang@paypal.com>
2024-01-01 22:04:15 +0800

    update

--- web/src/api/init.js
+++ web/src/api/init.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { rememberToken, getValidToken } from "./token";
 
-
-const baseURL = process.env.NODE_ENV === "development" ? "http://localhost:7000":process.env.BACKEND_BASE_URL;
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:7000"
+    : process.env.BACKEND_BASE_URL;
 // Create an axios instance
 const api = axios.create({
-  baseURL,
+  baseURL: process.env.BACKEND_BASE_URL,
 });
 
 export function setToken(token) {
